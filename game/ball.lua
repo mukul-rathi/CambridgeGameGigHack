2e205ba81804e36cc8daba5338fe59bdcf86831d
michaelng126 <michaelng126@users.noreply.github.com>
2017-12-01 16:07:18 +0000

    Add files via upload
    Main.lua:
    - added difficulty and difficulty increase variables. Difficulty increase causes speed to increase as game progresses.
    - did a few boundary improvements (e.g. stop coins going out of the game space)
    - renamed initial ball conditions from ball to init_ball for clarification
    - renamed images.coin to images.ball (as should be)
    - made player and init_ball variables versatile, by using getHeight and getWidth methods from the images themselves, rather than absolute values
    - added score variable, renamed fonts
    - added antiStick measure to player to prevent player 'sticking' to the top or bottom - cheating!

--- game/ball.lua
+++ game/ball.lua
@@ -2,9 +2,9 @@ function createBall()
 	local ball = {}
 	-- Initial Properties
 	ball.x = love.graphics.getWidth() - 50
-	ball.y = math.random(cave.top, cave.bottom - 50)-- how to get image height to replace the 50?
-	ball.sx = 0 - ball.speed
-	ball.sy = ball.speed
+	ball.y = math.random(cave.top, cave.bottom - images.ball:getHeight())
+	ball.sx = 0 - init_ball.speed
+	ball.sy = init_ball.speed
 
 	table.insert(balls, ball)
 end
--- game/main.lua
+++ game/main.lua
@@ -7,36 +7,44 @@ function love.load()
 	-- Is game over?
 	gameover = false
 
+	-- Difficulty rating (0 to 1) DIFFICULTY GOES UP EXPONENTIALLY!
+	-- Increase means increase in speed
+	difficulty = 0.04
+	difficultyIncrease = 0.5
+
 	-- Cave Height to control bouncing
 	cave = {}
 
-	cave.top = 200
-	cave.bottom = love.graphics.getHeight() - 200
+	cave.top = 100
+	cave.bottom = love.graphics.getHeight() - 150
 	cave.left = 50
 
-	-- Ball properties
-	ball = {}
-	ball.width = 20
-	ball.height = 20
-	ball.speed = 5
-
-	player = {}	
-	player.x = 50
-	player.y = love.graphics.getHeight()/2
-	player.w = 50
-	player.h = 50
-	player.jump = 10
+	-- Added new score
+	score = 0
 
 	balls = {}
 
 	fonts = {}
-	fonts.large = love.graphics.newFont("assets/Gamer.ttf",250)
+	fonts.score = love.graphics.newFont("assets/Gamer.ttf",36)
+	fonts.gameover = love.graphics.newFont("assets/Gamer.ttf", 250)
 
 	images = {}
 	images.background = love.graphics.newImage("assets/ground.png")
-	images.coin = love.graphics.newImage("assets/coin.png")
+	images.ball = love.graphics.newImage("assets/coin.png")
 	images.player = love.graphics.newImage("assets/player_down.png")
 	
+	player = {}	
+	player.x = 50
+	player.y = 210
+	player.w = images.player:getWidth()
+	player.h = images.player:getHeight()
+	player.jump = images.player:getHeight()
+
+	-- Each ball's Initial properties - init_ball
+	init_ball = {}
+	init_ball.width = images.ball:getWidth()
+	init_ball.height = images.ball:getWidth()
+	init_ball.speed = 5
 end
 
 
@@ -47,17 +55,16 @@ function love.update(dt)
 		return
 	end
 
-	if math.random() < 0.05 then
+	if math.random() < difficulty then
 		createBall()
 	end
-
 	
 	
 	-- Iterate through balls
 	for i=#balls, 1, -1 do
 		local ball = balls[i]
 
-		if AABB(player.x, player.y, player.w, player.h, ball.x , ball.y , ball.width , ball.height) then
+		if AABB(player.x, player.y, player.w, player.h, ball.x , ball.y , init_ball.width , init_ball.height) then
 			-- Player has collided, dead.
 			gameover = true
 			return
@@ -65,13 +72,16 @@ function love.update(dt)
 
 		if ball.x < cave.left then
 			-- ball has ended
-			table.remove(balls, i)		
+			table.remove(balls, i)
+			score = score + 1
 		else
 			
 			-- MODIFY BALL SPEED HERE --
-			if ball.y < cave.top or ball.y > cave.bottom then
+			if ball.y < cave.top or ball.y > cave.bottom - images.ball:getHeight() then
 				-- If collision, change y speed by reversing
 				ball.sy = 0 - ball.sy
+
+				init_ball.speed = init_ball.speed + difficultyIncrease
 			end
 			-- END MODIFY BALL SPEED HERE
 
@@ -87,7 +97,7 @@ function love.draw()
 
 	-- Background
 	for x = 0, love.graphics.getWidth(), images.background:getWidth() do
-		for y = cave.top, cave.bottom, images.background:getWidth() do
+		for y = cave.top, cave.bottom - images.background:getHeight(), images.background:getHeight() do
 			love.graphics.draw(images.background, x,y)
 		end
 	end
@@ -98,12 +108,14 @@ function love.draw()
 	-- Balls
 	for i=1, #balls, 1 do
 		local ball = balls[i]
-		love.graphics.draw(images.coin, ball.x, ball.y)
+		love.graphics.draw(images.ball, ball.x, ball.y)
 	end
 	
-	love.graphics.setFont(fonts.large)
+	love.graphics.setFont(fonts.score)
+	love.graphics.print("Score: " .. score, 10, 10)
 	--love.graphics.print(#balls, 20, 10)
 	if gameover then
+		love.graphics.setFont(fonts.gameover)
 		love.graphics.print("gameover", 10, 200)
 	end
 
@@ -111,12 +123,16 @@ function love.draw()
 end
 
 function love.keypressed(key)
-if key == "up" then
-		if player.y > cave.top then
+
+	-- Add an 'anti-sticking' measure to prevent players sticking to the top or bottom
+	antiStick = 30
+
+	if key == "up" then
+		if player.y > cave.top + antiStick then
 			player.y = player.y - player.jump
 		end
 	elseif key == "down" then
-		if player.y < cave.bottom then
+		if player.y < cave.bottom - images.ball:getHeight() - antiStick then
 			player.y = player.y + player.jump
 		end
 	end
